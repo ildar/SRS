6c4f4d3230773def76e039e6ead6c0f7ed1a42ad
Ildar Mulyukov <ildar@altlinux.ru>
2021-10-15 09:59:02 +0600

    СРС3: убрал определение интервалов

--- No3_piecewise_fn.py
+++ No3_piecewise_fn.py
@@ -2,31 +2,18 @@
 
 import math
 
-def interval_no(x):
+def fn(x):
     if x<-6:
-        return 0
+        return -3.0
     elif x<-3:
-        return 1
+        return x+3
     elif x<3:
-        return 2
+        return math.sqrt(3*3 - x*x)
     elif x<8:
-        return 3
-    else:
-        return 4
+        return (3.0/5)*(x-3)
+    elif interval == 4:
+        return 3.0
 
-# main
-x = float(input())
-
-interval = interval_no(x)
-if interval == 0:
-    y = -3.0
-elif interval == 1:
-    y = x+3
-elif interval == 2:
-    y = math.sqrt(3*3 - x*x)
-elif interval == 3:
-    y = (3.0/5)*(x-3)
-elif interval == 4:
-    y = 3
-
-print(y)
+if __name__ == '__main__':
+    x = float(input())
+    print(fn(x))
