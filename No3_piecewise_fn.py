3256fad56a73823e8f11e39c5254f151e24500a7
Ildar Mulyukov <ildar@altlinux.ru>
2021-10-18 10:33:09 +0600

    autopep8 reformatting

--- No3_piecewise_fn.py
+++ No3_piecewise_fn.py
@@ -2,17 +2,19 @@
 
 import math
 
+
 def fn(x):
-    if x<-6:
+    if x < -6:
         return -3.0
-    elif x<-3:
+    elif x < -3:
         return x+3
-    elif x<3:
+    elif x < 3:
         return math.sqrt(3*3 - x*x)
-    elif x<8:
+    elif x < 8:
         return (3.0/5)*(x-3)
     else:
         return 3.0
+
 
 if __name__ == '__main__':
     x = float(input())
--- No4_fn_tab.py
+++ No4_fn_tab.py
@@ -5,10 +5,10 @@ import No3_piecewise_fn
 
 if __name__ == '__main__':
     # x1,x2,dx = float(input()), float(input()), float(input())
-    x1,x2,dx = -4.0,9.0,0.5
+    x1, x2, dx = -4.0, 9.0, 0.5
     print('X', 'Y', sep='\t')
     print('=', '=', sep='\t')
     x = x1
-    while x<=x2:
+    while x <= x2:
         print(x, No3_piecewise_fn.fn(x), sep='\t')
         x = x + dx
